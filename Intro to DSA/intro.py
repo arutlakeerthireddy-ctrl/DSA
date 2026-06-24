5422ec27a7369c279c40a820b840f88f9c6c90f7
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-06-24 15:35:11 +0530

    added dsa introduction

---
+++ Intro to DSA/intro.py
@@ -0,0 +1,50 @@
+#what is DSA?
+'''DSA(Data structures and algorithms) is the study of how data is stored,organized,and processed efficiently in computer program
+*Data structure=a way to store and organize data
+*Algorithm=a step by step procedure to solve a problem'''
+#example
+'''imagine a library:
+bookshelves=data structure(how books are arranged)
+method to find a book=algorithm(how you search for it)'''
+#importance
+'''*Faster execution of programs
+*Better memory management
+*strong logical thinking
+*easier debugging
+*better coding interview performance
+*useful in real world,software development'''
+
+#Types of data structures
+#1.Linear data structure:Elements arranged sequentially
+#Array:stores similar data in contiguous memory
+#example:marks=[12,30,40]
+#Linked list:elements are connected using pointers
+#ex:10->20->30->40
+#stack(LIFO):last in,first out
+#queue(FIFO):first in,first out
+
+#2.NonLinear Data structures:Data is not arranged sequentially
+#Tree:Hierarchical structure
+#Graph:Represents connections btw objects
+#Hash table(dictionary):stores data as key value pairs
+
+#Types of Algorithms
+#Searching Algorithms:used to find data
+#1.Linear search:checks each element one by one
+#2.Binary search:Repeatedly divides sorted data into halves
+
+#sorting algorithms:used to arrange data
+#1.Bubble sort
+#2.selection sort
+#3.Insertion sort
+#4.merge sort
+#5.quick sort
+
+#Recursion:A function calling itself
+#Dynamic programming:breaks a problem into subproblems and stores result
+#ex:Fibonacci numbers,shortest path problems
+#Greedy algorithms:choose the best option at each step
+
+
+
+
