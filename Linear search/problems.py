a70d4b81a36f66000da50ae918f963a4d809ced2
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-06-26 21:50:36 +0530

    added

--- Linear search/problems.py
+++ Linear search/problems.py
@@ -10,4 +10,19 @@ def find_element(arr,target):
             return True
 myarr=[5,3,8,2]
 target=8
-print(find_element(myarr,target))
+print(find_element(myarr,target))#True
+
+'''Count Occurrences
+Count how many times a target appears.
+Example:
+Input: [2,3,2,4,2], target=2
+Output: 3'''
+def count_occurences(arr,target):
+    count=0
+    for i in range(len(arr)):
+        if arr[i]==target:
+            count+=1
+    return count
+li=[2,3,2,4,2]
+tar=2
+print(count_occurences(li,tar))#3
