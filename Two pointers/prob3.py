6782143a9678bf0e23b26f1d2e6f518e3481af6d
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-07-21 00:08:53 +0530

    added program

---
+++ Two pointers/prob3.py
@@ -0,0 +1,19 @@
+'''Move All Zeroes to the End
+Problem: Move all zeroes to the end while maintaining the relative order of non-zero elements.
+Example:
+Input:[0,1,0,3,12]
+Output:[1,3,12,0,0]'''
+def move_zeroes(arr):
+    if not arr:
+        return 0
+    slow=0
+    for fast in range(1,len(arr)):
+        if arr[fast]!=0:
+            arr[slow],arr[fast]=arr[fast],arr[slow]
+            slow+=1
+    return arr
+arr=[0,1,0,3,12]
+length=move_zeroes(arr)
+print(length)#[1, 3, 12, 0, 0]
+
+
