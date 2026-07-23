db81b2ac2ea7e8c51dfc95f8e47307ba265f95ec
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-07-23 13:39:53 +0530

    added

--- prefix sum/program1.py
+++ prefix sum/program1.py
@@ -27,3 +27,18 @@ def count_even(arr):
 arr=[4,9,2,1,3]
 print(count_even(arr))#2
 #space complexity:O(1)
+
+'''Count Odd Sum Prefixes
+Count how many prefix sums are odd.'''
+def count_odd(arr):
+    prefix_sum=0
+    count=0
+    for num in arr:
+        prefix_sum+=num
+        if prefix_sum%2!=0:
+            count+=1
+    return count
+arr=[4,9,2,1,3]
+print(count_odd(arr))#3
+
+
