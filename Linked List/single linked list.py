287ece153df8e33d401f7794e622ba28de2d7b35
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-08-04 15:03:48 +0530

    added single linked list

--- Linked List/single linked list.py
+++ Linked List/single linked list.py
@@ -23,3 +23,45 @@ assign head and cur:O(1)
 Traverse and print all nodes:O(n)'''
 #space complexity:O(n)
 #Auxiliary space complexity:O(1)
+
+#Insertion at beginning:new node becomes the new head
+'''
+before
+10->20->30
+after inserting 5
+5->10->20->30
+
+#steps
+# create new node
+# point new node to current node
+# update head'''
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+n1=Node(10)
+n2=Node(20)
+n3=Node(30)
+n1.next=n2
+n2.next=n3
+head=n1
+print("Before insertion:")
+cur=head
+while cur:
+    print(cur.data,end="->")
+    cur=cur.next
+print('None')
+new=Node(5)
+new.next=head
+head=new
+print('After insertion:')
+cur=head
+while cur:
+    print(cur.data,end="->")
+    cur=cur.next
+print("None")
+'''
+Before insertion:
+10->20->30->None
+After insertion:
+5->10->20->30->None'''
