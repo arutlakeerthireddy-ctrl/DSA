41a51f821cb151456c10ba6e6135546bc347c650
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-08-20 20:54:55 +0530

    added

--- Interview/problem5.py
+++ Interview/problem5.py
@@ -1,4 +1,4 @@
-#Reverse an Integer Write a function to reverse the digits of a given integer.
+#5.Reverse an Integer Write a function to reverse the digits of a given integer.
 def Rev_Int(num):
     num1=str(num)
     return int(num1[::-1])
