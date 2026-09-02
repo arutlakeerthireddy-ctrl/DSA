d058ecc0a1d75a4a2dd42eb6439c7aab90277584
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-09-02 18:10:23 +0530

    added Trees

---
+++ Trees/Intro.py
@@ -0,0 +1,30 @@
+#Trees in DSA:
+'''A tree is a non linear data structure used to represent hierarchical relationships btw elements
+#Basic Terminology
+# root-the topmost node
+# node-An individual element in a tree
+# Edge-connection btw two nodes
+# parent-a node that has children
+# child-A node connected below parent
+# Leaf-A node with no children
+# Height-Longest path from a node to a leaf
+# Depth-distance from root to node
+# subtree-A tree formed from a node and its descendents
+
+#Types
+Binary tree 
+Binary search tree
+AVL tree
+Heap
+Trie
+N-ary Tree
+
+#Real world applications
+# File systems
+# HTML/XML DOM
+# search engines
+# databases
+# compilers
+# Ai $ml
+# organization charts
+# games '''
