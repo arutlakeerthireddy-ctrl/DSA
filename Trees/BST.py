f2fc187376203b1fcc5c933d317f96eb6511070d
arutlakeerthireddy-ctrl <arutlakeerthireddy@gmail.com>
2026-09-18 00:24:16 +0530

    added

--- Trees/BST.py
+++ Trees/BST.py
@@ -17,8 +17,31 @@ root.left=Node(30)
 root.right=Node(70)
 root.left.left=Node(10)
 root.left.right=Node(40)
-print(search(root,70))#TrueS
-    
-    
-        
-        
+print(search(root,70))#True
+
+#BST insertiom
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    if key<root.data:
+        root.left=insert(root.left,key)
+    elif key>root.data:
+        root.right=insert(root.right,key)
+    return root
+root=Node(50)
+root.left=Node(30)
+root.right=Node(70)
+root.left.left=Node(10)
+root.left.right=Node(40)
+root=insert(root,65)
+print(root.right.left.data)#65
+'''
+Complexity
+Balanced BST: O(log N)
+Skewed BST: O(N)
+Recursion space: O(H)'''
